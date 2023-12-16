b407340048bad1f6483e29aac2eb74c015277eee
sarthakmalikmeerut <66469025+sarthakmalikmeerut@users.noreply.github.com>
2023-12-17 00:52:35 +0530

    Update dlib_attendence.py

--- dlib_attendence.py
+++ dlib_attendence.py
@@ -1,93 +1,5 @@
-# import cv2
-# import numpy as np
-# import face_recognition
-# import os
-# from datetime import datetime
-#
-# path = 'training_images'
-# images = []
-# classNames = []
-# myList = os.listdir(path)
-# print(myList)
-# for cl in myList:
-#     curImg = cv2.imread(f'{path}/{cl}')
-#     images.append(curImg)
-#     classNames.append(os.path.splitext(cl)[0])
-# print(classNames)
-#
-#
-# def findEncodings(images):
-#     encodeList = []
-#
-#     for img in images:
-#         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#         encode = face_recognition.face_encodings(img)[0]
-#         encodeList.append(encode)
-#     return encodeList
-#
-#
-# def markAttendance(name):
-#     with open('Attendance.csv', 'r+') as f:
-#         myDataList = f.readlines()
-#
-#         nameList = [line.split(',')[0] for line in myDataList]
-#         if name not in nameList:
-#             now = datetime.now()
-#             dtString = now.strftime('%H:%M:%S')
-#             f.writelines(f'\n{name},{dtString}')
-#             print(f'Attendance marked for {name} at {dtString}')
-#         else:
-#             pass
-#
-#
-# def clearAttendance():
-#     with open('Attendance.csv', 'w') as f:
-#         f.truncate(0)
-#     print('Attendance cleared.')
-#
-#
-# encodeListKnown = findEncodings(images)
-# print('Encoding Complete')
-#
-# cap = cv2.VideoCapture(1)
-#
-# while True:
-#     success, img = cap.read()
-#     imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-#     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-#
-#     facesCurFrame = face_recognition.face_locations(imgS)
-#     encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
-#
-#     for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-#         matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-#         faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-#         matchIndex = np.argmin(faceDis)
-#
-#         if matches[matchIndex]:
-#             name = classNames[matchIndex].upper()
-#             y1, x2, y2, x1 = faceLoc
-#             y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-#             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-#             cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-#             cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-#             markAttendance(name)
-#
-#     cv2.imshow('Webcam', img)
-#
-#     # Check for key press
-#     key = cv2.waitKey(1) & 0xFF
-#     if key == ord('e'):
-#         clearAttendance()
-#
-#     # Break the loop if 'Esc' key is pressed
-#     elif key == 27:
-#         break
-#
-# cap.release()
-# cv2.destroyAllWindows()
-
 import cv2
+import speech_recognition as sr
 import numpy as np
 import face_recognition
 import os
@@ -120,7 +32,7 @@ else:
         encodeList = []
         for img in images:
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            encode = face_recognition.face_encodings(img)[0]
+            encode = face_recognition.face_encodings(img, num_jitters=1)[0]
             encodeList.append(encode)
         return encodeList
 
@@ -133,59 +45,129 @@ else:
 
 print('Encoding Complete')
 
+
+recognizer = sr.Recognizer()
+microphone = sr.Microphone()
+# ... (previous code)
+
+def start_listening():
+    with microphone as source:
+        print("Say 'Mark Attendance' to start recognition...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        command = recognizer.recognize_google(audio).lower()
+        print(f"Command: {command}")
+        return "mark attendance" in command
+    except sr.UnknownValueError:
+        return False
+    except sr.RequestError as e:
+        print(f"Speech Recognition request failed: {e}")
+        return False
+
 cap = cv2.VideoCapture(1)
 
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
+    now = datetime.now()
+    date_string = now.strftime('%d_%m_%y_%H%M')  # Include hour and minute in the filename
+    file_name = f'{date_string}.csv'
+
+    # Open the file in 'a+' mode (append and read)
+    with open(file_name, 'a+') as f:
+        # Move the file cursor to the beginning for reading
+        f.seek(0)
+
+        # Read all lines to check for existing names
         myDataList = f.readlines()
 
+        # Check if the name is already in the file
         nameList = [line.split(',')[0] for line in myDataList]
         if name not in nameList:
-            now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
-            print(f'Attendance marked for {name} at {dtString}')
-        else:
-            pass
+            print(f'Attendance marked for {name} at {dtString} on {date_string}.')
 
 def clearAttendance():
     with open('Attendance.csv', 'w') as f:
         f.truncate(0)
     print('Attendance cleared.')
 
+# Main Loop
+# ... (previous code)
+
+# ... (previous code)
+
+# Main Loop
 while True:
-    success, img = cap.read()
-    imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+    # Start listening for the command
+    if start_listening():
+        print("Recognition started. Press 'Esc' to exit.")
 
-    facesCurFrame = face_recognition.face_locations(imgS)
-    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+        # Mark attendance only once
+        attendance_marked = False
 
-    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        matchIndex = np.argmin(faceDis)
+        # Counter to track consecutive frames with the same person
+        consistent_frame_count = 0
+        consistent_frame_threshold = 5  # Set the threshold as per your requirement
 
-        if matches[matchIndex]:
-            name = classNames[matchIndex].upper()
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
-            markAttendance(name)
+        while True:
+            success, img = cap.read()
+            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    cv2.imshow('Webcam', img)
+            facesCurFrame = face_recognition.face_locations(imgS)
+            encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    # Check for key press
+            for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+                matchIndex = np.argmin(faceDis)
+
+                if matches[matchIndex]:
+                    name = classNames[matchIndex].upper()
+                    y1, x2, y2, x1 = faceLoc
+                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+
+                    # Check if the detected person is consistent for consecutive frames
+                    if consistent_frame_count >= consistent_frame_threshold:
+                        if not attendance_marked:
+                            markAttendance(name)
+                            attendance_marked = True
+                        attendance_marked= False
+                        consistent_frame_count = 0
+                    else:
+                        consistent_frame_count += 1
+
+                else:
+                    # Reset the counter if the person changes
+                    attendance_marked = False
+                    consistent_frame_count = 0
+
+            cv2.imshow('Webcam', img)
+
+            # Check for key press
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('e'):
+                clearAttendance()
+
+            # Break the loop if 'Esc' key is pressed
+            elif key == 27:
+                # Reset attendance_marked and consistent_frame_count when breaking the loop
+                attendance_marked = False
+                consistent_frame_count = 0
+                break
+
+    else:
+        print("Recognition not started. Press 'Esc' to exit.")
+
+    # Check for key press outside the attendance recognition loop
     key = cv2.waitKey(1) & 0xFF
-    if key == ord('e'):
-        clearAttendance()
-
-    # Break the loop if 'Esc' key is pressed
-    elif key == 27:
+    if key == 27:
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
