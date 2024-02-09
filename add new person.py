6ff30710bfba163ab759686108ac2eeb013120df
sarthakmalikmeerut <66469025+sarthakmalikmeerut@users.noreply.github.com>
2024-02-10 01:09:20 +0530

    Update add new person.py

--- add new person.py
+++ add new person.py
@@ -3,6 +3,13 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 from datetime import datetime
+import threading
+import subprocess
+import os
+import face_recognition
+import pickle
+
+
 
 class CameraApp:
     def __init__(self, window, window_title, video_source=0):
@@ -32,8 +39,33 @@ class CameraApp:
         ret, frame = self.cap.read()
         if ret:
             file_name = input()
-            cv2.imwrite(file_name, frame)
+            save_path = os.path.join('training_images', f'{file_name}.jpg')
+            cv2.imwrite(save_path, frame)
+
             print(f"Image captured and saved as {file_name}")
+        path = 'training_images'
+        images = []
+        classNames = []
+        myList = os.listdir(path)
+        print(myList)
+
+        for cl in myList:
+            curImg = cv2.imread(f'{path}/{cl}')
+            images.append(curImg)
+            classNames.append(os.path.splitext(cl)[0])
+        print(classNames)
+
+        # Load saved encodings if available, otherwise create and save them
+        encodings_file = 'encodings.pkl'
+
+        encodeListKnown = []
+        for image in images:
+            face_encodings = face_recognition.face_encodings(image)
+            if len(face_encodings) > 0:
+                encodeListKnown.append(face_encodings[0])
+            with open(encodings_file, 'wb') as f:
+                pickle.dump(encodeListKnown, f)
+        print('Encodings saved to file.')
 
     def convert_to_tkinter_image(self, frame):
         rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -44,6 +76,14 @@ class CameraApp:
     def __del__(self):
         if self.cap.isOpened():
             self.cap.release()
+    def start_external_script(self):
+        external_script_thread = threading.Thread(target=self.run_external_script)
+        external_script_thread.start()
+
+    def run_external_script(self):
+        script_path = 'add new person.py'  # Replace with the actual path to your script
+        subprocess.run(['python', script_path])
+
 
 if __name__ == "__main__":
     # Specify the video source (use 0 for the default camera)
